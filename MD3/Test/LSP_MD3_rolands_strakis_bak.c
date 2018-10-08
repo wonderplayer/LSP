ab6c16ee6cbf8a4c51eccfbce7b4262b783f8d8d
Rolands Strakis <roland.strakis@gmail.com>
2018-10-08 20:49:37 +0300

    Pabeigts MD3 uz 60%

--- MD3/Test/LSP_MD3_rolands_strakis_bak.c
+++ MD3/Test/LSP_MD3_rolands_strakis_bak.c
@@ -4,7 +4,6 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 typedef enum {true, false} bool;
 typedef struct Node Node;
@@ -56,60 +55,11 @@ void FreeTree(Node *p){
 	free(p);
 }
 
-void PrintDuplicates(){
-	Duplicate *d = duplicates;
-	DuplicateFile *df = d->df;
-	struct stat buf;
-	int error;
-	char timeBuffer[26];
-	struct tm *tm_info;
-	while(d != NULL){
-		df = d->df;
-		printf("Ver: '%s'\n", df->path);
-		error = stat(df->path, &buf);
-		if(error < 0){
-			perror("PrintDuplicates: ");
-		}
-		tm_info = localtime(&buf.st_mtime);
-		strftime(timeBuffer, 26, "%Y-%m-%d %H:%M", tm_info);
-		printf("=== %s %ld %s\n", timeBuffer, buf.st_size, df->name);
-		while(df != NULL){
-			printf("%s\n", df->path);
-			df = df->next;
-		}
-		printf("\n");
-		d = d->next;
-
-	}
-}
-
-void FreeDuplicateFile(DuplicateFile *df){
-	free(df->path);
-	free(df->name);
-	df->path = NULL;
-	df->name = NULL;
-	free(df);
-}
-
-void FreeDuplicates(){
-	Duplicate *d = duplicates;
-	DuplicateFile *df = d->df;
-	while(d != NULL){
-		while(df != NULL){
-			df = d->df->next;
-			FreeDuplicateFile(d->df);
-			d->df = df;
-		}
-		d = duplicates->next;
-		free(duplicates);
-		duplicates = d;
-	}
-}
-
 void AddDuplicateRecord(char *path, char *name, Duplicate *p){
+	DuplicateFile *df = NULL;
 	printf("Ieksa AddDuplicateRecord\n");
 	printf("Liek ieksa '%s/%s'\n", path, name);
-	DuplicateFile *df = p->df;
+	df = p->df;
 	while(df->next != NULL) df = df->next;
 	df->next = malloc(sizeof(DuplicateFile));
 	df = df->next;
@@ -122,65 +72,40 @@ void AddDuplicateRecord(char *path, char *name, Duplicate *p){
 	strcpy(df->name, name);
 }
 
-void AddDuplicate(char *path, char *name, Node *n){
+void AddDuplicate(char *path, char *name){
 	printf("Ieksa AddDuplicate\n");
 	Duplicate *p = duplicates;
-	Duplicate *prev = duplicates;
-	int res;
 	if(p->df == NULL){
 		printf("Veido pirmo failu duplikatu ierakstu failam '%s'\n", name);
-		p->next = NULL;
 		p->df = malloc(sizeof(DuplicateFile));
 		p->df->next = NULL;
-		p->df->path = malloc(strlen(n->path) + strlen(n->name));
-		p->df->name = malloc(strlen(n->name));
-		strcpy(p->df->path, n->path);
+		p->df->path = malloc(strlen(path) + strlen(name));
+		p->df->name = malloc(strlen(path));
+		strcpy(p->df->path, path);
 		strcat(p->df->path,"/");
-		strcat(p->df->path, n->name);
-		strcpy(p->df->name, n->name);
-		p->df->next = malloc(sizeof(DuplicateFile));
-		p->df->next->next = NULL;
-		p->df->next->path = malloc(strlen(path) + strlen(name));
-		p->df->next->name = malloc(strlen(name));
-		strcpy(p->df->next->path, path);
-		strcat(p->df->next->path,"/");
-		strcat(p->df->next->path, name);
-		strcpy(p->df->next->name, name);
+		strcat(p->df->path, name);
+		strcpy(p->df->name, name);
 	}
 	else {
-		while(p != NULL){
-			printf("Salidzina '%s' ar '%s' ", p->df->name, name);
-			res = strcmp(p->df->name, name);
-			printf("%d\n", res);
-			if(strcmp(p->df->name, name) == 0){
+		while(p->next != NULL){
+			if(p->df->name == name){
 				AddDuplicateRecord(path, name, p);
 				return;
 			}
-			printf("%ld\n", p);
-			printf("%ld\n", p->next);
-			prev = p;
 			p = p->next;
 		}
 		printf("Pievieno duplikatus failam '%s'\n", name);
-		p = malloc(sizeof(Duplicate));
+		p->next = malloc(sizeof(Duplicate));
+		p = p->next;
 		p->next = NULL;
 		p->df = malloc(sizeof(DuplicateFile));
 		p->df->next = NULL;
-		p->df->path = malloc(strlen(n->path) + strlen(n->name));
-		p->df->name = malloc(strlen(n->name));
-		strcpy(p->df->path, n->path);
+		p->df->path = malloc(strlen(path) + strlen(name));
+		p->df->name = malloc(strlen(name));
+		strcpy(p->df->path, path);
 		strcat(p->df->path,"/");
-		strcat(p->df->path, n->name);
-		strcpy(p->df->name, n->name);
-		p->df->next = malloc(sizeof(DuplicateFile));
-		p->df->next->next = NULL;
-		p->df->next->path = malloc(strlen(path) + strlen(name));
-		p->df->next->name = malloc(strlen(name));
-		strcpy(p->df->next->path, path);
-		strcat(p->df->next->path,"/");
-		strcat(p->df->next->path, name);
-		strcpy(p->df->next->name, name);
-		prev->next = p;
+		strcat(p->df->path, name);
+		strcpy(p->df->name, name);
 	}
 }
 
@@ -218,6 +143,7 @@ void AddFileToRecord(char *path, char *name, unsigned char tabCount){
 				strcpy(fullCheckFilePath, p->path);
 				strcat(fullCheckFilePath, "/");
 				strcat(fullCheckFilePath, p->name);
+				printf("Path: '%s'\n", fullCheckFilePath);
 				existsCheckFile = stat(fullCheckFilePath, &checkFile);
 				if(existsCheckFile < 0 || existsCurrentFile < 0){
 					perror("Problema ar faila atversanu: ");
@@ -225,7 +151,7 @@ void AddFileToRecord(char *path, char *name, unsigned char tabCount){
 				}
 				else {
 					if(currentFile.st_size == checkFile.st_size){
-						AddDuplicate(path, name, p);
+						AddDuplicate(path, name);
 						return;
 					}
 					else{
@@ -268,7 +194,7 @@ void FindFilePath(DIR *d, char *prevPath, unsigned char tabCount){
 		else if((strcmp(de->d_name, ".")) != 0 && (strcmp(de->d_name, "..")) != 0 && de->d_type == 4){
 			PrintTabs(tabCount);
 			printf("Atrada direktoriju '%s'\n", de->d_name);
-			strcpy(dirName, prevPath);
+			strcat(dirName, prevPath);
 			strcat(dirName, "/");
 			strcat(dirName, de->d_name);
 			PrintTabs(tabCount);
@@ -281,6 +207,7 @@ void FindFilePath(DIR *d, char *prevPath, unsigned char tabCount){
 				perror(dirName);
 			}
 		}
+
 	}
 
 	closedir(d);
@@ -316,7 +243,6 @@ int main(int argc, char **argv){
 	PrintTreePreOrder(tree);
 	FreeTree(tree);
 	PrintDuplicates();
-	FreeDuplicates();
 	tree = NULL;
 
 	return 0;
